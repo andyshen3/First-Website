7b9397012d48f8f016172dcc5916f91d7c1e8455
andyshen3 <andyshen2882@gmail.com>
2017-08-28 11:09:31 -0700

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,168 @@
+/*
+   
+    Template Name : Rolling - Freelancer Portfolio Template
+    Author : UiPasta Team
+    Website : http://www.uipasta.com/
+    Support : http://www.uipasta.com/support/
+	
+	
+*/
+
+
+
+/*
+   
+   Table Of Content
+   
+   1. Preloader
+   2. Smooth Scroll
+   3. Scroll Naviagation Background Change with Sticky Navigation
+   4. Mobile Navigation Hide or Collapse on Click
+   5. Scroll To Top
+   6. Typed.js
+   7. Parallax Background
+   8. Portfolio Filtering
+
+*/
+
+
+(function ($) {
+    'use strict';
+
+    jQuery(document).ready(function () {
+
+        
+       /* Preloader */
+		
+        $(window).on('load', function() {
+          $('body').addClass('loaded');
+        });
+		
+		
+		
+       /* Smooth Scroll */
+
+        $('a.smoth-scroll').on("click", function (e) {
+            var anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $(anchor.attr('href')).offset().top - 50
+            }, 1000);
+            e.preventDefault();
+        });
+		
+
+
+       
+       /* Scroll Naviagation Background Change with Sticky Navigation */
+		 
+        $(window).on('scroll', function () {
+            if ($(window).scrollTop() > 100) {
+                $('.header-top-area').addClass('navigation-background');
+            } else {
+                $('.header-top-area').removeClass('navigation-background');
+            }
+        });
+		
+		
+		
+		
+       /* Mobile Navigation Hide or Collapse on Click */
+		
+        $(document).on('click', '.navbar-collapse.in', function (e) {
+            if ($(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle') {
+                $(this).collapse('hide');
+            }
+        });
+        $('body').scrollspy({
+            target: '.navbar-collapse',
+            offset: 195
+        
+		 });
+		 
+		
+		
+		
+        /* Scroll To Top */
+		
+        $(window).scroll(function(){
+        if ($(this).scrollTop() >= 500) {
+            $('.scroll-to-top').fadeIn();
+         } else {
+            $('.scroll-to-top').fadeOut();
+         }
+	   });
+	
+	
+	    $('.scroll-to-top').click(function(){
+		  $('html, body').animate({scrollTop : 0},800);
+		  return false;
+	    });
+		
+		
+		
+		
+        /* Typed.js */
+		
+        $(window).load(function(){
+        $(".typing").typed({
+    strings: ["I am a junior at Carnegie Mellon University studying Electrical and Computer Engineering.", "I enjoy playing basketball.", "I am interested in learning about new technologies."],
+    typeSpeed: 10,
+    backDelay: 2500,
+    loop: !0, // here
+    startDelay: 1000,
+});
+         });
+        
+		 
+        /* Parallax Background */
+
+        $(window).stellar({
+            responsive: true,
+            horizontalScrolling: false,
+            hideDistantElements: false,
+            horizontalOffset: 0,
+            verticalOffset: 0,
+        });
+
+        
+		
+		
+        /* Portfolio Filtering */
+
+        $('.portfolio-inner').mixItUp();
+
+
+       
+        /* Magnific Popup */
+
+        $('.portfolio-popup').magnificPopup({
+            type: 'image',
+			
+            gallery: { enabled: true },
+			zoom: { enabled: true,
+			        duration: 500
+					
+          },
+		  
+         image:{
+               markup: '<div class="mfp-figure portfolio-pop-up">'+
+               '<div class="mfp-close"></div>'+
+               '<div class="mfp-img"></div>'+
+               '<div class="mfp-bottom-bar portfolio_title">'+
+               '<div class="mfp-title"></div>'+
+               '<div class="mfp-counter"></div>'+
+               '</div>'+
+               '</div>',
+
+               titleSrc:function(item){
+                return item.el.attr('title');
+              }
+            }
+		  
+		  
+          });
+              
+		   
+            });
+
+   })(jQuery);
